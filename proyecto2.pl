be006cda7e1bd2261c5e523ea2cfac6dd42dc0e3
yosergiovc <104660842+yosergiovc@users.noreply.github.com>
2022-06-15 18:10:41 -0500

    Update proyecto2.pl
    -Quité la palabra "Funcion" de los comentarios porque en Prolog no hay funciones, me parece que son reglas que definen el comportamiento de predicados pero no escribí eso para cambiar el comentario lo menos posible.
    -Quité lo que era "comer_queso" y cambié el nombre de lo que era "tipo_queso" por "comer_queso" porque no creí que el paso intermedio fuera necesario.
    -Cambié uno de los hechos de lo que antes era "tipo_queso" y ahora es "comer_queso" porque me parece que decía "normal" donde debía decir "vino".

--- proyecto2.pl
+++ proyecto2.pl
@@ -19,7 +19,7 @@ estado_auxiliar((Xi,Yi),Oi,[C|T],Est_Raton,Pf,Of,_) :-
     estado_auxiliar(P1,O1,T,Est_Raton,Pf,Of,_).
 
 
-/**Funcion que simula un paso al frente 
+/**Simula un paso al frente 
 que hace el raton sin alterar su orientacion*/
 movimiento((Xi,Yi),north,avanzar,Est_Raton,(Xi,Yf),north) :-
     Yf is Yi+1.
@@ -33,42 +33,42 @@ movimiento((Xi,Yi),east,avanzar,Est_Raton,(Xf,Yi),east) :-
 movimiento((Xi,Yi),west,avanzar,Est_Raton,(Xf,Yi),west) :-
     Xf is Xi-1.
 
-/**Funcion que hace girar hacia la izquierda al raton, 
+/**Hace girar hacia la izquierda al raton, 
 modificando la orientacion*/
 
 movimiento((Xi,Yi),Oi,giraI,Est_Raton,(Xi,Yi),Of) :-
     giro_izq(Oi,giraI,Of).
 
-/**Funcion que hace girar hacia la derecha al raton, así como su orientacion*/
+/**Hace girar hacia la derecha al raton, así como su orientacion*/
 
 movimiento((Xi,Yi),Oi,giraD,Est_Raton,(Xi,Yi),Of) :-
     giro_der(Oi,giraD,Of).
 
-/**Funcion que hace al raton dar media vuelta, de acuerdo a su orientacion*/
+/**Hace al raton dar media vuelta, de acuerdo a su orientacion*/
 
 movimiento((Xi,Yi),Oi,gira180,Est_Raton,(Xi,Yi),Of) :-
     giro180(Oi,gira180,Of).
 
-/**Funcion para que el raton pueda comer un queso*/
+/**Hace al raton comer un queso*/
 
 movimiento((Xi,Yi),Oi,comer_queso,Est_Raton,(Xi,Yi),Of) :-
     comer_queso(Est_Raton,T_Queso,Estado_Raton_F).
 
-/**Funcion auxiliar que realiza las respectivas rotaciones 
+/**Auxiliar que realiza las respectivas rotaciones 
 de acuerdo a la direccion que está mirando el raton*/
 giro180(north,gira180,south).
 giro180(south,gira180,north).
 giro180(east,gira180,west).
 giro180(west,gira180,east).
 
-/**Funcion auxiliar que realiza las respectivas 
+/**Auxiliar que realiza las respectivas 
 rotaciones hacia la izquierda de acuerdo a la direccion que está mirando el raton*/
 giro_izq(north,giraI,west).
 giro_izq(south,giraI,east).
 giro_izq(east,giraI,north).
 giro_izq(west,giraI,south).
 
-/**Funcion auxiliar que realiza las respectivas rotaciones 
+/**Auxiliar que realiza las respectivas rotaciones 
 hacia la derecha de acuerdo a la direccion que está mirando el raton*/
 
 giro_der(north,giraD,east).
@@ -76,20 +76,15 @@ giro_der(south,giraD,west).
 giro_der(east,giraD,south).
 giro_der(west,giraD,north).
 
-/**Funcion auxiliar que revisa el tipo de queso a 
-comer y devolviendo el estado que tiene el raton despues de consumirlo*/
-comer_queso(Estado_Raton_I,T_Queso,Estado_Raton_F) :-
-    tipo_queso(Estado_Raton_I,T_Queso,Estado_Raton_F).
 
-
-/**Base de conocimientos que muestra el comportamiento 
+/**Auxiliar que muestra el comportamiento 
 del raton luego de comer un determinado queso de acuerdo a su estado actual*/
-tipo_queso(sobrio,normal,sobrio).
-tipo_queso(ebrio,normal,ebrio).
-tipo_queso(sobrio,vino,ebrio).
-tipo_queso(ebrio,normal,ebrio).
-tipo_queso(ebrio,veneno,muerto).
-tipo_queso(sobrio,veneno,sobrio).
+comer_queso(sobrio,normal,sobrio).
+comer_queso(ebrio,normal,ebrio).
+comer_queso(sobrio,vino,ebrio).
+comer_queso(ebrio,vino,ebrio).
+comer_queso(ebrio,veneno,muerto).
+comer_queso(sobrio,veneno,sobrio).
 
 /**
 Base de conocimientos que figura las acciones que puede hacer 
@@ -101,7 +96,7 @@ accion(2,giraD).
 accion(3,gira180).
 
 /**
-Funcion que genera un movimiento al azar de acuerdo 
+Genera un movimiento al azar de acuerdo 
 a la base de conocimientos implementado anteriormente
 */
 generarMovimiento(A):- random(0,3,X),accion(X,A).
